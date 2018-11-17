3cc1cc357a72c3f0e4655ecf604991bbc8ef158c
Jakub Florczak <44369186+flopczak@users.noreply.github.com>
2018-11-17 20:50:12 +0100

    przełałem client

--- client.py
+++ client.py
@@ -4,6 +4,10 @@ HOST = 'localhost'
 PORT = 9999
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+    while True :
+        temp = input()
+        bitowo = temp.encode()
+        s.send(bitowo)
+        data = s.recv(1024)
+        print('Received', repr(data))
+
