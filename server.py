4b67274b84e9db1ea1bccfb944061202c4c85922
Bartłomiej Olszanowski <baloo@int.pl>
2018-11-17 19:24:10 +0100

    Added server boilerplate

---
+++ server.py
@@ -0,0 +1,30 @@
+import socketserver
+from protocol import Turbo
+
+
+DEBUG = True
+
+
+class TurboProtocolTCPHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        print("Entered handle")
+        while True:
+            data = self.request.recv(4096)
+            word = data.decode()
+            if word == "\r\n":
+                print("ENTER")
+                continue
+            elif word == "":
+                break
+
+            print(word)
+            word = word + "\r\n"
+            self.request.sendall(word.encode())
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    with socketserver.TCPServer((HOST, PORT), TurboProtocolTCPHandler) as server:
+        server.serve_forever(5.0)
