c4517dafeac2e9e295c7a5eb27d7ddcbb97b6c19
Jakub Florczak <44369186+flopczak@users.noreply.github.com>
2018-11-19 16:26:01 +0100

    poprawione ale nadal NOT musi być po liczbie

--- client.py
+++ client.py
@@ -4,7 +4,8 @@ from protocol import Turbo
 
 
 DEBUG = True
-
+MIN = -2147483648
+MAX = 2147483647
 
 def debugger(msg):
     if DEBUG:
@@ -23,6 +24,7 @@ if __name__ == "__main__":
         while True:
 
             #Wysyłanie
+            print("Please always write sign after first number and all separated space: ")
             eq = input()
             if eq == "EXIT" or eq == "Exit" or eq == "exit":
                 s.shutdown(socket.SHUT_RDWR)
@@ -34,30 +36,21 @@ if __name__ == "__main__":
                 print("Too many arguments. Expected 3 arguments. ")
                 continue
             elif len(eq) == 3:
-                if int(eq[0]) > 2147483647 or int(eq[0]) < -2147483648 or int(eq[2]) > 2147483647 or int(eq[2]) < -2147483648:
+                first = int(eq[0])
+                second = int(eq[2])
+                sign = eq[1]
+                if first > MAX or first < MIN or second > MAX or second < MIN:
                     print("number is too big! expected number between -2147483648 and 2147483647 ")
                     continue
-                else:
-                    first = int(eq[0])
-                    second = int(eq[2])
-                    sign = eq[1]
+
             elif len(eq) == 2:
-                if type(eq[0])== int and (eq[1]=="!" or  eq[1]=="NOT"):
-                    if int(eq[0]) > 2147483647 or int(eq[0]) < -2147483648:
+                sign = eq[1]
+                first = int(eq[0])
+                second = 0
+                if type(eq[0]) == int and (eq[1] == "!" or  eq[1] == "NOT"):
+                    if int(eq[0]) > MAX or int(eq[0]) < MIN:
                         print("number is too big! expected number between -2147483648 and 2147483647 ")
                         continue
-                    else:
-                        sign = eq[1]
-                        first = int(eq[0])
-                        second = 0
-                if type(eq[1])== int and (eq[0]=="!" or  eq[0]=="NOT"):
-                    if int(eq[1]) > 2147483647 or int(eq[1]) < -2147483648:
-                        print("number is too big! expected number between -2147483648 and 2147483647 ")
-                        continue
-                    else:
-                        sign = eq[1]
-                        first = int(eq[0])
-                        second = 0
 
 
             temp_items = [sign, first, second]
